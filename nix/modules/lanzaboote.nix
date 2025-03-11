ae2f3598d3f4dba3537ea87a883504ae92286b04
Jean-Baptiste Giraudeau <jb@giraudeau.info>
2025-03-11 14:03:35 +0100

    Add keepCurrentBootedConfiguration option
     to always keep booted system configuration in boot menu.

--- nix/modules/lanzaboote.nix
+++ nix/modules/lanzaboote.nix
@@ -35,6 +35,8 @@ in
       '';
     };
 
+    keepCurrentBootedConfiguration = lib.mkEnableOption "Always keep current booted configuration (with labeled 'SAFE' in boot menu)";
+
     pkiBundle = lib.mkOption {
       type = lib.types.nullOr lib.types.path;
       description = "PKI bundle containing db, PK, KEK";
@@ -125,20 +127,22 @@ in
         ${lib.optionalString cfg.enrollKeys ''
           ${lib.getExe' pkgs.coreutils "mkdir"} -p /tmp/pki
           ${lib.getExe' pkgs.coreutils "cp"} -r ${cfg.pkiBundle}/* /tmp/pki
-          ${lib.getExe sbctlWithPki} enroll-keys --yes-this-might-brick-my-machine
-        ''}
+            ${lib.getExe sbctlWithPki} enroll-keys --yes-this-might-brick-my-machine
+          ''}
 
-        # Use the system from the kernel's hostPlatform because this should
-        # always, even in the cross compilation case, be the right system.
-        ${lib.getExe cfg.package} install \
-          --system ${config.boot.kernelPackages.stdenv.hostPlatform.system} \
-          --systemd ${config.systemd.package} \
-          --systemd-boot-loader-config ${loaderConfigFile} \
-          --public-key ${cfg.publicKeyFile} \
-          --private-key ${cfg.privateKeyFile} \
-          --configuration-limit ${toString configurationLimit} \
-          ${config.boot.loader.efi.efiSysMountPoint} \
-          /nix/var/nix/profiles/system-*-link
+          # Use the system from the kernel's hostPlatform because this should
+          # always, even in the cross compilation case, be the right system.
+          ${lib.getExe cfg.package} install \
+            --system ${config.boot.kernelPackages.stdenv.hostPlatform.system} \
+            --systemd ${config.systemd.package} \
+            --systemd-boot-loader-config ${loaderConfigFile} \
+            --public-key ${cfg.publicKeyFile} \
+            --private-key ${cfg.privateKeyFile} \
+            --configuration-limit ${toString configurationLimit} \
+            ${lib.optionalString cfg.keepCurrentBootedConfiguration ''
+            --safe-generation /run/booted-system \
+        ''} ${config.boot.loader.efi.efiSysMountPoint} \
+            /nix/var/nix/profiles/system-*-link
       '';
     };
 
